70d24f95879fe9b67a029a1a60641862c3e62c75
Kite <ixkaito@gmail.com>
2023-06-12 16:34:30 +0900

    Update packages

--- .prettierrc.js
+++ .prettierrc.js
@@ -4,4 +4,13 @@
 module.exports = {
   singleQuote: true,
   semi: true,
+  plugins: [require.resolve('prettier-plugin-astro')],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
 };
